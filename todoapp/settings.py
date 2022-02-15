b859e8af33b2215c9acdb14245c875bc6b4312cb
Spencer555 <46141953+Spencer555@users.noreply.github.com>
2022-02-15 01:38:30 +0000

    Update settings.py

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!@#!#$%$^&&*(*&(^(*&^(SDGDHDBVCXCZXnk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://spl-django-todoapp.herokuapp.com/', '127.0.0.1:8000', 'localhost:8000', 'https://spl-django-todoapp.heroku.com/']
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
